8e7b10853feb11695ee7f3343169d8bb7b2291f4
gong.rg <2nEoluUva0ED520>
2021-01-11 20:24:39 +0800

    register add rules

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,26 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import register from "../components/register";
+import Pro_index from "../components/pro_index";
+import intro from "../components/intro";
+import search from '../components/search';
+import login from "../components/login";
 
 Vue.use(VueRouter)
 
 const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+  //主页，默认路由
+  {path: '/', redirect: '/Pro_index'},
+  //产品主页
+  {path: '/Pro_index', name: Pro_index, component: Pro_index},
+  //关于
+  {path: '/intro', name: intro, component: intro},
+  //注册页
+  {path: '/register', component: register},
+  //搜索页
+  {path: '/search', component: search},
+  //登录页
+  {path: '/login', component: login},
 ]
 
 const router = new VueRouter({
